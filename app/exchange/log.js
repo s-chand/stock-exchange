25b897920c2c5bbb5c24b46c875cbe3bfd2d1a6d
Samuel Okoroafor <samuel.i.okoroafor@gmail.com>
2018-03-23 18:53:16 +0100

    fix log to file

--- app/exchange/log.js
+++ app/exchange/log.js
@@ -1,16 +1,25 @@
 const fs = require("fs");
+const os = require("os");
 
 //  Write the output to file for cases wheen
 
 const writeToLogFile = message => {
     // TODO: handle file writing to the logs folder using the fs module based on NODE_ENV
+    try {
+        fs.appendFileSync('/tmp/logs/log.txt', message + os.EOL);
+      } catch (err) {
+        /* Handle the error */
+        console.log(err)
+      }
 };
 exports.logger = (resultCollection, check) => {
     const messageToLog = `${check}: ${resultCollection.join(",")}`;
+    writeToLogFile(messageToLog)
     console.log(messageToLog);
 };
 
 exports.winnerLog = winner => {
     const messageToLog = `Winner = ${winner.CompanyID}`;
+    writeToLogFile(messageToLog)
     console.log(messageToLog);
 };
